83a1b3fdbd3504fd6302d284a658fe7e85c30a6a
rubenmartinmorris <rubenmartinmorris@gmail.com>
2021-11-29 16:28:43 +0000

    first two inserts working correctly

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,6 @@
-const db = require("./");
+const db = require("../connection");
+const format = require("pg-format");
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
   return db
@@ -7,7 +9,7 @@ const seed = (data) => {
       return db.query(`DROP TABLE IF EXISTS articles`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS toppics `);
+      return db.query(`DROP TABLE IF EXISTS topics `);
     })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS users`);
@@ -31,24 +33,60 @@ const seed = (data) => {
         title VARCHAR(255) NOT NULL,
         body VARCHAR(255) NOT NULL,
         votes INT DEFAULT 0,
-        topic VARCHAR(255), 
+        topic VARCHAR(255)  , 
         author VARCHAR(255),
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP(5)
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP(5),
+        FOREIGN KEY (topic) REFERENCES topics (slug),
+        FOREIGN KEY (author) REFERENCES users (username)
       )`);
-      // topic: field which references the slug in the topics table
+      // topic: field which references the slug in the topics table foreigh key constraint
       // author: field that references a user's primary key (username)
     })
     .then(() => {
-      return dbb.query(`CREATE TABLE comments(
+      return db.query(`CREATE TABLE comments(
         comment_id SERIAL PRIMARY KEY,
         author VARCHAR(255),
         article_id INT,
         votes INT DEFAULT 0,
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP(5),
-        body VARCHAR (255)
+        body VARCHAR (255),
+        FOREIGN KEY (author) REFERENCES users (username),
+        FOREIGN KEY (article_id) REFERENCES articles(article_id)
       )`);
-      //field that references a user's primary key (username)
+      //author field that references a user's primary key (username)
       //field that references an article's primary key
+    })
+    .then(() => {
+      const queryString = format(
+        `
+      INSERT INTO users (
+        username, avatar_url, name
+      )
+      VALUES 
+      %L
+      `,
+        userData.map((user) => [user.username, user.avatar_url, user.name])
+      );
+      return db.query(queryString);
+    })
+    .then(() => {
+      const queryString = format(
+        `
+      INSERT INTO topics (
+        slug,description
+      )
+      VALUES
+      %L
+      RETURNING *
+      `,
+        topicData.map((topic) => [topic.slug, topic.description])
+      );
+      //console.log(queryString);
+
+      return db.query(queryString);
+    })
+    .then((lol) => {
+      console.log(lol.rows);
     });
 };
 
