c0e7f9278d75ed2c23457b938baa1f5f11972908
rubenmartinmorris <rubenmartinmorris@gmail.com>
2021-12-01 16:36:52 +0000

    sql test files added and shortcut to package.json

--- test.sql
+++ test.sql
@@ -1,5 +1,7 @@
 \c nc_news_test
-
+/*
 SELECT * FROM articles WHERE articles.topic = 'cats';
 
 SELECT comment_id,votes,created_at,author,body FROM comments WHERE comments.article_id = 1;
+*/
+INSERT INTO comments(body,votes,author,article_id) VALUES ('lol',0,'butter_bridge',3) RETURNING *
