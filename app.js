f8dc328c1da7e8027d1cf35cdcb13f942c6c4d53
rubenmartinmorris <rubenmartinmorris@gmail.com>
2021-12-02 12:37:53 +0000

    app.get added to app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const apiRouter = require("./routers/api.router");
+const apiRouter = require('./routers/api.router');
 
 app.use(express.json());
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
+
+app.get('/', (req, res, next) => {
+  res.status(200).send({ msg: 'Hello from my news api :)' });
+});
 
 module.exports = app;
